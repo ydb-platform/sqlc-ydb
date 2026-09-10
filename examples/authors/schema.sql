1af7298e0f070d729d2236fa42d2b20c69bbbc27
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-09-10 17:44:40 +0300

    schema.sql

--- examples/authors/schema.sql
+++ examples/authors/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE authors (
-    id Uint64 NOT NULL,
-    name Utf8 NOT NULL,
-    bio Utf8,
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS authors (
+   id Uint64 NOT NULL,
+   name Utf8 NOT NULL,
+   bio Utf8,
+   PRIMARY KEY (id)
 );
